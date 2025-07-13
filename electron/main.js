557b0a4b568926cb32da5e4bbfda35be8d641b99
Yumeth <yumethsumathipala@gmail.com>
2025-07-13 07:03:18 +0530

    Fix main window creation in Electron app
    The input fields not focusing bug on windows using built installer is addressed.

--- electron/main.js
+++ electron/main.js
@@ -8,22 +8,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 function createWindow() {
-  const win = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     fullscreen: true,
     minimizable: false,
     maximizable: false,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: false,
+      contextIsolation: true,
       devTools: false,
     },
   });
 
   // Prevent leaving fullscreen via keyboard shortcuts or menu
-  win.on('leave-full-screen', () => {
-    win.setFullScreen(true);
+  mainWindow.on('leave-full-screen', () => {
+    mainWindow.setFullScreen(true);
   });
 
-  win.loadFile(path.join(__dirname, '../dist/index.html'));
+  mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+  mainWindow.focus();
 }
 
 app.whenReady().then(createWindow);
